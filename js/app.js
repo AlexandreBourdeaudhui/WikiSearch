1667ba4edb6a1a346014648e9f6b2a44066a918c
AlexandreB <alexandre.bourdeaudhui@gmail.com>
2016-12-12 14:45:27 +0100

    Modif

--- js/app.js
+++ js/app.js
@@ -5,13 +5,40 @@ var resultElt = document.getElementById('result');
 // Request Ajax
 //ajaxGet('https://fr.wikipedia.org/w/api.php?action=opensearch&search=' + searchElt + '&limit=10', function (response) {
 // On recupere en json les 15 mots qui se rapprochent le plus de la requete souhaitée (paris)
-ajaxGet('https://en.wikipedia.org/w/api.php?action=query&format=json&gsrlimit=15&generator=search&origin=*&gsrsearch=' + searchElt, function (response) {
-var result = JSON.parse(response).query.pages;
-  for(var index in result) { 
-    var attr = result[index]; 
-    // Regarde ta console ce que tu obtiens :)
-    console.log(attr);
-  }
+ajaxGet('https://fr.wikipedia.org/w/api.php?action=query&format=json&generator=search&origin=*&prop=extracts&exchars=500&exintro=true&exlimit=15&gsrlimit=15&gsrsearch=' + searchElt, function (response) {
+    var result = JSON.parse(response).query.pages;
+    console.log(result);
+
+    for (var x in result) {
+        var attr = result[x];
+
+        var searchContent = {
+            title: attr.title,
+            extract: attr.extract,
+            pageid: attr.pageid
+        };
+
+        // Regarde ta console ce que tu obtiens :)
+        // console.log(attr);
+        var titreElt = searchContent.title;
+        var extractElt = searchContent.extract;
+        var pageidElt = searchContent.pageid;
+
+        var titleLi = document.createElement('li');
+        titleLi.textContent = titreElt;
+
+        var extractP = document.createElement('p');
+        extractP.innerHTML = extractElt;
+
+        var pageidA = document.createElement('a');
+            pageidA.href = "https://en.wikipedia.org/?curid=" + pageidElt;
+            pageidA.textContent = "https://en.wikipedia.org/wiki/" + titreElt;
+
+        resultElt.appendChild(titleLi);
+        resultElt.appendChild(extractP);
+        resultElt.appendChild(pageidA);
+    }
+
 });
 
 /**
@@ -19,21 +46,14 @@ var result = JSON.parse(response).query.pages;
  * @param  {object}
  * @return {string} 
  */
-function generateBlock (obj) {
-  // a clean - je le laisse pour te rappeler ce que tu avais fais et pas tout supprimer
- /* var titre = document.createElement('p');
-  titre.textContent = result[0];
-
-  var descrip = document.createElement('p');
-  descrip.textContent = result[1];
-
-  var url = document.createElement('a');
-  url.textContent = result[2];
-
-  resultElt.appendChild(titre);
-  resultElt.appendChild(descrip);
-  resultElt.appendChild(url);
-  */
+function generateBlock(obj) {
+    /* On créé un block par défaut qui contiendras :
+    - un titre,
+    - un bref texte qui présentera le titre
+    - l'url
+    */
+    var divElt = document.createElement('div');
+    console.log(searchContent.title);
 };
 
 /**
@@ -41,6 +61,8 @@ function generateBlock (obj) {
  * @id  {integer}
  * @return url {string} 
  */
-function getUrl (id) {
-
+function getUrl(id) {
+  
 };
+
+generateBlock(result);
