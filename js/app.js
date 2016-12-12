c3c9879f8e52c3b6589ae3c6fdb1628d25e9cacc
AlexandreB <alexandre.bourdeaudhui@gmail.com>
2016-12-12 18:54:25 +0100

    Modification fichier JS/HTML/CSS

--- js/app.js
+++ js/app.js
@@ -1,68 +1,75 @@
-var searchElt = "paris";
+/* @Todo :
+- Trier les résultats par leur index
+- Enlever les balises <p> qui sont dans l'extract
+- Voir pour des URLs plutôt que des ID
+- Mettre un message si recherche NOK || recherche OK
+
+*/
 var resultElt = document.getElementById('result');
-//document.querySelector('[input type="search"]');
 
-// Request Ajax
-//ajaxGet('https://fr.wikipedia.org/w/api.php?action=opensearch&search=' + searchElt + '&limit=10', function (response) {
-// On recupere en json les 15 mots qui se rapprochent le plus de la requete souhaitée (paris)
-ajaxGet('https://fr.wikipedia.org/w/api.php?action=query&format=json&generator=search&origin=*&prop=extracts&exchars=500&exintro=true&exlimit=15&gsrlimit=15&gsrsearch=' + searchElt, function (response) {
-    var result = JSON.parse(response).query.pages;
-    console.log(result);
+var formElt = document.querySelector('form');
+formElt.addEventListener('submit', function (e) {
+    e.preventDefault();
+    var searchElt = formElt.elements.search.value;
 
-    for (var x in result) {
-        var attr = result[x];
+    // Request Ajax
+    // Request done in JSON format with ~10 result & params 
+    ajaxGet('https://fr.wikipedia.org/w/api.php?action=query&format=json&generator=search&origin=*&prop=extracts&exchars=500&exintro=true&exlimit=10&gsrlimit=10&gsrsearch=' + searchElt, function (response) {
+        var result = JSON.parse(response).query.pages;
+        console.log(result);
 
-        var searchContent = {
-            title: attr.title,
-            extract: attr.extract,
-            pageid: attr.pageid
-        };
+        for (var x in result) {
+            var attr = result[x];
 
-        // Regarde ta console ce que tu obtiens :)
-        // console.log(attr);
-        var titreElt = searchContent.title;
-        var extractElt = searchContent.extract;
-        var pageidElt = searchContent.pageid;
+            var searchContent = {
+                title: attr.title,
+                extract: attr.extract,
+                pageid: attr.pageid,
+                index: attr.index
+            };
 
-        var titleLi = document.createElement('li');
-        titleLi.textContent = titreElt;
 
-        var extractP = document.createElement('p');
-        extractP.innerHTML = extractElt;
+            /**
+             * return preformated block of content
+             * @param  {object}
+             * @return {string} 
+             */
 
-        var pageidA = document.createElement('a');
-            pageidA.href = "https://en.wikipedia.org/?curid=" + pageidElt;
-            pageidA.textContent = "https://en.wikipedia.org/wiki/" + titreElt;
+            function generateBlock(obj) {
+                var divContainer = document.createElement('div');
+                divContainer.classList.add('show-content');
 
-        resultElt.appendChild(titleLi);
-        resultElt.appendChild(extractP);
-        resultElt.appendChild(pageidA);
-    }
+                var titleA = document.createElement('a');
+                titleA.href = "https://fr.wikipedia.org/?curid=" + searchContent.pageid;
+                titleA.textContent = searchContent.title;
 
+                var extractSpan = document.createElement('span');
+                extractSpan.innerHTML = searchContent.extract;
+
+                var pageidLi = document.createElement('li');
+                pageidLi.textContent = "https://fr.wikipedia.org/wiki/" + searchContent.title;
+
+                resultElt.appendChild(divContainer);
+                divContainer.appendChild(titleA);
+                divContainer.appendChild(pageidLi);
+                divContainer.appendChild(extractSpan);
+
+            };
+            // If Search == Ok
+            if (searchElt != undefined) {
+                generateBlock();
+            }
+        }
+    });
+    resultElt.innerHTML = ""; // Delete result when new Search
 });
-
-/**
- * return preformated block of content
- * @param  {object}
- * @return {string} 
- */
-function generateBlock(obj) {
-    /* On créé un block par défaut qui contiendras :
-    - un titre,
-    - un bref texte qui présentera le titre
-    - l'url
-    */
-    var divElt = document.createElement('div');
-    console.log(searchContent.title);
-};
 
 /**
  * return preformated url
  * @id  {integer}
  * @return url {string} 
  */
-function getUrl(id) {
-  
-};
 
-generateBlock(result);
+function getUrl(id) {
+
+};
